b1df94087760fb46c452300e3c43265455c24ff3
Denise Souza <95714393+Denisessouza@users.noreply.github.com>
2022-07-26 01:07:47 -0300

    Cards de Ofertas alocados

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,10 @@
 function MostrarMenu(){
+    let menu_01 = document.getElementById ("menu")
+    if (getComputedStyle(menu).display =='none'){ 
+                menu_01.style.display='flex';
+        
+    }    else {
+        menu_01.style.display="none";
+    }  
     
 }
