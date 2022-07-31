3c57c381cabb1daa46de3d57932ace75121b7835
Denise Souza <95714393+Denisessouza@users.noreply.github.com>
2022-07-30 21:56:18 -0300

    Implementações JQuery

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,37 @@
+$ (document).ready(function(){
+
+    let slideAtual = 1;
+    let listaSlides = ["banner_principal", "banner_corredor", "banner_mago"]
+
+        function mudarSlides(){
+
+            if(slideAtual > 0){
+                $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+                
+            } else if (slideAtual == 0){
+                $("#carrossel").removeClass(listaSlides[(listaSlides.length) - 1])
+            }
+        }
+
+        $("#barras").click(function(){
+
+            $("#menu_01").toggleClass("menu_01-ativo");
+
+
+          
+
+            // if($("#menu_01").hasClass("menu_01-ativo")) {
+            //     $("#menu_01").removeClass("menu_01-ativo")
+            // } else {
+            //     $("#menu_01").addClass("menu_01-ativo")
+            // }
+        })
+})
+
+
+
+
+
 
 function mostrarMenu() {
     let menu_01 = document.getElementById ("menu_01")
